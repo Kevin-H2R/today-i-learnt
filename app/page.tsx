636516875b61e42bd55761dd08f913c7e3f42221
Kevin <herrbach.kevin@gmail.com>
2024-06-15 12:56:36 +0900

    Fixes a bit layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,22 @@ const Home = async () => {
   const post = await getLastPost()
   console.log("post")
   console.log(post)
+  const today = new Date()
+  const hoursDiff = (today.getTime() - post!.date.getTime()) / (1000 * 60 * 60)
+  const formattedDate = post!.date.getFullYear() + '-' +
+    post!.date.getMonth().toString().padStart(2, '0') + '-' +
+    post!.date.getDate().toString().padStart(2, '0')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col gap-8">
-        <div className="w-full max-w-5xl items-center text-2xl text-center">
+      <div className="flex flex-col gap-8 w-full xl:w-8/12">
+        <div className="w-full items-center text-2xl text-center">
           Hi, I'm Kevin and I want to learn something new everyday.
         </div>
         <div className="w-full max-w-5xl items-center text-xl text-center">
-          Here's what I learnt today:
+          Here's what I learnt { hoursDiff > 24 ? `on the ${formattedDate}` : 'today' }:
         </div>
         <TestButton/>
-        {post && 
+        {post &&
         <div className="max-w p-6 border rounded-xl bg-white shadow">
           <div className="text-center">
             {post?.title}
