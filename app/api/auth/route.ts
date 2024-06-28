3b82176a780b76dc6b45ba86430a1e4554fddc7d
Kevin <herrbach.kevin@gmail.com>
2024-06-28 15:40:52 +0900

    Safely checks for env variable presence

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,6 +6,9 @@ import { cookies } from "next/headers";
 const POST = async (request: Request) => {
   const {username, password} = await request.json()
   const user = await prisma.user.findFirst({ where: { username } })
+  if (!process.env.SECRET_KEY) {
+    return Response.json({message: "error"}, {status: 401})
+  }
   if (user && await bcrypt.compare(password, user.password)) {
     const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY)
     cookies().set("token", token)
