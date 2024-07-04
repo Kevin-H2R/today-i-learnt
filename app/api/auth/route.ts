f2799022e368186799619a53adcfe6f231a89fca
Kevin <herrbach.kevin@gmail.com>
2024-07-04 10:41:30 +0900

    Details login error in logs

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -4,17 +4,21 @@ import jwt from "jsonwebtoken"
 import { cookies } from "next/headers";
 
 const POST = async (request: Request) => {
-  const {username, password} = await request.json()
-  const user = await prisma.user.findFirst({ where: { username } })
+  const { username, password } = await request.json()
+
   if (!process.env.SECRET_KEY) {
-    return Response.json({message: "error"}, {status: 401})
+    return Response.json({ message: "Could not find the secret key" }, { status: 401 })
   }
-  if (user && await bcrypt.compare(password, user.password)) {
+  const user = await prisma.user.findFirst({ where: { username } })
+  if (!user){
+    return Response.json({message: "Could not find the user"}, {status: 401})
+  }
+  if (await bcrypt.compare(password, user.password)) {
     const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY)
     cookies().set("token", token)
     return Response.json({})
   }
-  return Response.json({message: "error"}, {status: 401})
+  return Response.json({message: "Incorrect password"}, {status: 401})
 }
 
 export {POST}
