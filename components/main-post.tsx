a214569bdb784d874483b284d7d7c5989697fd65
Kevin <herrbach.kevin@gmail.com>
2024-07-09 17:38:59 +0900

    Tries something to avoid the 500 error in prod

--- components/main-post.tsx
+++ components/main-post.tsx
@@ -3,10 +3,16 @@
 import { usePostContext } from "@/context/post-context"
 import Markdown from "react-markdown"
 import ShareButton from "./share-button"
+import { useEffect } from "react"
 
  const MainPost = () => {
-  const {mainPost} = usePostContext()
-  fetch('/api/view', { method: 'PUT', body: JSON.stringify({ postId: mainPost.id }) })
+   const { mainPost } = usePostContext()
+   useEffect(() => {
+      const addView = async () => {
+        await fetch('/api/view', { method: 'PUT', body: JSON.stringify({ postId: mainPost.id }) })
+      }
+      addView()
+   })
 
   return mainPost && <div className="p-6 border rounded-xl bg-white shadow">
     <div className="text-center text-4xl">
