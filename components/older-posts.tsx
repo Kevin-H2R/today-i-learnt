594607f700cdf82b66af5374757239a382161de1
Kevin <herrbach.kevin@gmail.com>
2024-07-09 17:33:55 +0900

    Prevents moving to same post with the older post link

--- components/older-posts.tsx
+++ components/older-posts.tsx
@@ -4,15 +4,16 @@ import { usePostContext } from "@/context/post-context"
 import { Post } from "@prisma/client"
 
 const OlderPosts = () => {
-  const { posts, setMainPost } = usePostContext()
+  const { posts, mainPost, setMainPost } = usePostContext()
 
   const changePost = async (post: Post) => {
+    if (post.id === mainPost.id) return
     const response = await fetch('/api/view', { method: 'PUT', body: JSON.stringify({ postId: post.id }) })
     console.log(response)
     setMainPost(post)
   }
   return <ul>
-  {posts && posts.map(p =>
+  {posts && posts.map(p => p.id === mainPost.id ? null :
   <li className="text-gray-500 cursor-pointer" key={'post_' + p.id}
   onClick={() => {changePost(p)}}>{p.title}</li>
   )}
